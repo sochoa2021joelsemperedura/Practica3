586592c8739550b9bebf9bab802dcd3c44d4ebde
Joel Sempere <joelsempere96@gmail.com>
2021-10-13 19:39:44 +0200

    Practica 3, iniciandola

---
+++ app/src/main/java/com/example/practica3/NuevoContactoActivity.java
@@ -0,0 +1,14 @@
+package com.example.practica3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevoContactoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_contacto);
+    }
+}
