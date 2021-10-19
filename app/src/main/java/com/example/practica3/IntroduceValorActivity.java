e0ed65fbd4ddcb2dba3a92391565a66e81403ac6
JoelS <joelsempere96@gmail.com>
2021-10-19 17:56:42 +0200

    Revisar

--- app/src/main/java/com/example/practica3/IntroduceValorActivity.java
+++ app/src/main/java/com/example/practica3/IntroduceValorActivity.java
@@ -2,24 +2,45 @@ package com.example.practica3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class IntroduceValorActivity extends AppCompatActivity {
+    //Los extras que identificaran los datos que recibira de IntroduceValorActivity
+    public final static String EXTRA_NOMBRE_CONTACTO = "NuevoContactoActivity.NOMBRE";
+    public final static String EXTRA_APELLIDO_CONTACTO = "NuevoContactoActivity.APELLIDOS";
+    public final static String EXTRA_EMPRESA_CONTACTO = "NuevoContactoActivity.EMPRESA";
+
     EditText etValorNombreApellidos;
-    Button btnCancel;
-    Button btnOk;
+    Button btnCancelIntroduce;
+    Button btnOkIntroduce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduce_valor);
+
+        //inicia las views
+        iniciaViews();
+
+        //Boton para salir de la actividad
+        btnCancelIntroduce.setOnClickListener(e->{
+            finish();
+        });
+
+        btnOkIntroduce.setOnClickListener(e->{
+            //TODO Voy por aqui, revisar y solucionar, las constantes habra que cambiarlas
+            Intent intent = new Intent(IntroduceValorActivity.this,NuevoContactoActivity.class);
+            etValorNombreApellidos.getText().toString();
+
+        });
     }
 
     private void iniciaViews(){
         etValorNombreApellidos = findViewById(R.id.etValorNombreApellidos);
-        btnCancel = findViewById(R.id.btnCancel);
-        btnOk = findViewById(R.id.btnOk);
+        btnCancelIntroduce = findViewById(R.id.btnCancelIntroduce);
+        btnOkIntroduce = findViewById(R.id.btnOkIntroduce);
     }
 }
--- app/src/main/java/com/example/practica3/NuevoContactoActivity.java
+++ app/src/main/java/com/example/practica3/NuevoContactoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,11 +12,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NuevoContactoActivity extends AppCompatActivity {
-    //Los extras que identificaran los datos que recibira de IntroduceValorActivity
-    //SON OPTION REQUEST MIRAR EN GITHUB EL STARTACTIVITYFORREQUEST Y RECTIFICAR
-    public final static String EXTRA_NOMBRE_CONTACTO = "NuevoContactoActivity.NOMBRE";
-    public final static String EXTRA_APELLIDO_CONTACTO = "NuevoContactoActivity.APELLIDOS";
-    public final static String EXTRA_EMPRESA_CONTACTO = "NuevoContactoActivity.EMPRESA";
+
+
+    public final static int OPTION_REQUEST_NOMBRE = 0;
+    public final static int OPTION_REQUEST_APELLIDO = 1;
+    public final static int OPTION_REQUEST_EMPRESA = 2;
 
     /*Estas views se tendran que enviar a la main activity y si las clicas te llevaran
         a introduce introduceValorActivity (Ponerlas en clicable true)
@@ -23,6 +24,9 @@ public class NuevoContactoActivity extends AppCompatActivity {
     TextView tvNombreContacto;
     TextView tvApellidos;
     TextView tvEmpresa;
+    //El Seekbar, la textView representa a values de la barra (maximo establecido en modo diseño)
+    SeekBar skbEdad;
+    TextView tvEdadSkb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +35,49 @@ public class NuevoContactoActivity extends AppCompatActivity {
 
         //Inicio las views
         iniciaViews();
+
+        //el clic en cualquiera de las text view llama a la actividad introduceValorActivity y ademas se identifica a si misma para recibir una string de regreso si se da OK.
+        tvNombreContacto.setOnClickListener(e->{
+            iniciaActivityIntroducir(OPTION_REQUEST_NOMBRE,IntroduceValorActivity.EXTRA_NOMBRE_CONTACTO,tvNombreContacto.getText().toString());
+
+        });
+        tvApellidos.setOnClickListener(e->{
+            iniciaActivityIntroducir(OPTION_REQUEST_APELLIDO,IntroduceValorActivity.EXTRA_APELLIDO_CONTACTO,tvApellidos.getText().toString());
+        });
+        tvEmpresa.setOnClickListener(e->{
+            iniciaActivityIntroducir(OPTION_REQUEST_EMPRESA,IntroduceValorActivity.EXTRA_EMPRESA_CONTACTO,tvEmpresa.getText().toString());
+        });
+
+        skbEdad.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                //añade en la textView de la edad el valor del seekbar segun desplazas la barra
+                tvEdadSkb.setText(getString(R.string.stEdad)+" "+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
     //Inicia las views
     private void iniciaViews(){
         tvNombreContacto = findViewById(R.id.tvNombreContacto);
         tvApellidos = findViewById(R.id.tvApellidos);
         tvEmpresa = findViewById(R.id.tvEmpresa);
-        //RELLENAR PERO HACER PRIMERO LOS OBJETOS EN LA CLASE
+        skbEdad = findViewById(R.id.skbEdad);
+        tvEdadSkb = findViewById(R.id.tvEdadSkb);
     }
-
-    private void iniciaActivityIntroducir(){
+    //Metodo que recibe un intent y identifica que textView hace la llamada y establece el texto recibido de la otra actividad
+    private void iniciaActivityIntroducir(int optionRequest,String extra,String texto){
         Intent intent = new Intent(NuevoContactoActivity.this,IntroduceValorActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent,optionRequest);
+        intent.putExtra(extra,texto);
     }
 }
